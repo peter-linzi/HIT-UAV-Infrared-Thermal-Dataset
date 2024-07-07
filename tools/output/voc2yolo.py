36fff1976f5b896c98ee77489e602d04465b611b
linfanyu <linfanyu@aeronitor.com>
2024-07-07 17:17:32 +0800

    feat: convert voc to yolo

--- tools/output/voc2yolo.py
+++ tools/output/voc2yolo.py
@@ -1,11 +1,13 @@
 import xml.etree.ElementTree as ET
 import os
+import shutil
+
 
 # The script is based on https://github.com/AlexeyAB/darknet/blob/master/scripts/voc_label.py
 
 sets = ['train', 'val', 'test']
 
-classes = ["Person", "Car", "Bicycle", "OtherVehicle", "DontCare"]
+classes = ["Car", "OtherVehicle"]
 
 
 def convert(size, box):
@@ -22,9 +24,9 @@ def convert(size, box):
     return (x, y, w, h)
 
 
-def convert_annotation(image_id):
+def convert_annotation(image_id, set):
     in_file = open('Annotations/%s.xml' % image_id)
-    out_file = open('labels/%s.txt' % image_id, 'w')
+    out_file = open('labels/%s/%s.txt' % (set, image_id), 'w')
     tree = ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
@@ -36,7 +38,7 @@ def convert_annotation(image_id):
         cls = obj.find('name').text
         if cls not in classes or int(difficult) == 1:
             continue
-        cls_id = classes.index(cls)
+        cls_id = 0
         xmlbox = obj.find('bndbox')
         b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
              float(xmlbox.find('ymax').text))
@@ -44,15 +46,18 @@ def convert_annotation(image_id):
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
 
 
-wd = 'D:/dataset/with_js/VOC'
+wd = '.'
 
 for image_set in sets:
-    if not os.path.exists('labels/'):
-        os.makedirs('labels/')
+    if not os.path.exists('images/%s' % (image_set)):
+        os.makedirs('images/%s' % (image_set))
+    if not os.path.exists('labels/%s' % (image_set)):
+        os.makedirs('labels/%s' % (image_set))
     image_ids = open('ImageSets/Main/%s.txt' % image_set).read().strip().split()
-    list_file = open('%s.txt' % image_set, 'w')
+    # list_file = open('%s.txt' % image_set, 'w')
     for image_id in image_ids:
-        list_file.write('%s/JPEGImages/%s.jpg\n' % (wd, image_id))
-        convert_annotation(image_id)
-    list_file.close()
+        # list_file.write('%s/JPEGImages/%s.jpg\n' % (wd, image_id))
+        convert_annotation(image_id, image_set)
+        shutil.copy2("JPEGImages/%s.jpg" % image_id, "images/%s/%s.jpg"%(image_set, image_id))
+    # list_file.close()
 
